35c1451a17e69fa132adc0e7014b7e13d6edeb55
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-10-25 21:15:06 +0300

    Add files via upload

---
+++ files.py
@@ -0,0 +1,20 @@
+i = 0
+j = 0
+mas = []
+
+for line in open("in.txt", "r").read().split("\n"):
+    mas.append([])
+    for unit in line.split(" "):
+        mas[i].append(unit)
+        j += 1
+    i += 1
+    
+print(mas)
+
+f = open('out.txt', 'w')
+
+for i in range(len(mas)):
+    for j in range(len(mas[i])):
+        f.write(mas[i][j] + ' ')
+    f.write('\n')
+f.close()

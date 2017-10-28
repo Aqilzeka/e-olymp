821ec630628aa452a3955daac7b893071a68120e
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-10-28 20:55:38 +0300

    Add files via upload

---
+++ #909.py
@@ -0,0 +1,15 @@
+f = open("input.txt", "r").read().split("\n")
+n = int(f[0])
+mas = f[1].split(" ")
+line = ''
+k = 0
+template = '{:.2f}'
+
+for i in range(n):
+    if ((i+1) % 3 == 0) and float(mas[i]) > 0:
+        line = line + template.format(float(mas[i])) + ' '
+        k += 1
+        
+w = open("output.txt", "w")
+w.write(str(k) + ' ' + line)
+w.close()

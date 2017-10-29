c6b04b87d7e0af73209cb2b2f7a0bfa0e2ef140e
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-10-29 22:01:16 +0200

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,17 @@
+def sortByAlphabet(inputStr):
+        return inputStr[0]
+
+def sortByLength(inputStr):
+        return len(inputStr)
+    
+sortList = ['a', 'cc', 'bbb']
+sortList.sort(key=sortByAlphabet)
+print(sortList)
+
+sortList = ['a', 'cc', 'bbb']
+sortList.sort(key=sortByLength)
+print(sortList)
+
+sortList = ['a', 'cc', 'bbb']
+sortList.sort(key=sortByLength, reverse=True)
+print(sortList)

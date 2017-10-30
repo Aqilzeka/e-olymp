25399ba402734a63dfedc6d24f04df14fad1e185
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-10-30 16:21:45 +0200

    Add files via upload

---
+++ #147.py
@@ -0,0 +1,10 @@
+import datetime
+a = input()
+b = input()
+a = a.split(' ')
+b = b.split(' ')
+aa = datetime.date(int(b[2]),int(b[1]),int(b[0]))
+bb = datetime.date(int(a[2]),int(a[1]),int(a[0]))
+cc = aa-bb
+dd = str(cc)
+print(dd.split()[0])

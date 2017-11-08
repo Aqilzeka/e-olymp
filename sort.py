51d55d86112afcb1d02b9253f30f27e8c7c9773b
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-11-08 19:56:35 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -4,13 +4,16 @@ def sortByAlphabet(inputStr):
 def sortByLength(inputStr):
         return len(inputStr)
     
+        
 sortList = ['a', 'cc', 'bbb']
 sortList.sort(key=sortByAlphabet)
 print(sortList)
 
+
 sortList = ['a', 'cc', 'bbb']
 sortList.sort(key=sortByLength)
 print(sortList)
+
 
 sortList = ['a', 'cc', 'bbb']
 sortList.sort(key=sortByLength, reverse=True)

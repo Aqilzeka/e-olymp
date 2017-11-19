7a594b215e4cc671347fa2f4767d073bcd3dd1e4
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-11-19 23:50:12 +0200

    Велика точність

--- #11.py
+++ #11.py
@@ -1,10 +1,10 @@
 f = open("input.txt", "r").read().split(" ")
+w = open("output.txt", "w")
 
 m = int(f[0])
 n = int(f[1])
 k = int(f[2])
 template = '{:.3f}'
 
-w = open("output.txt", "w")
 w.write(template.format(float(m / n)))
 w.close()

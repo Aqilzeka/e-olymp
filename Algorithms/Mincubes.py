fa5960c9122fd93054ab47261af94fd948ce2e31
Myroslav Trigubets <myroslav.trigubets@gmail.com>
2017-12-19 14:53:25 +0200

    Add files via upload

--- Algorithms/Mincubes.py
+++ Algorithms/Mincubes.py
@@ -1,13 +1,37 @@
-import math
-n = int(input())
-a = 0
-for i in range(int(math.sqrt(n))):
-    for j in range(int(math.sqrt(n))):
-        for y in range(int(math.sqrt(n))):
-            if (i**3 + j**3 + y ** 3) == n:
-                a = [i,j,y]
+def is_int(n):
+    try:
+        return int(n) == float(n)
+    except ValueError:
+        return -1
 
-if a != 0:
-    print(a)
+def v(a):
+    if a != 0:
+        return a
+
+n = int(input())
+mass = []
+first = n
+for i in range(int(n**(1/3))):
+    k_cub = n**(1/3)
+    #print(i, int(k_cub), n )
+    if is_int(k_cub) == False:
+        mass.append(int(k_cub))
+
+        n -= int(int(k_cub)**3)
+    elif is_int(k_cub) == True:
+        mass.append(int(k_cub))
+        n -= int(int(k_cub)**3)
+
+
+if sum([i**3 for i in mass]) == first:
+    print(mass)
+elif sum([i**3 for i in mass]) + 1 == first:
+    mass.append(1)
+    print(mass)
 else:
-    print(-1)
+    mass = -1
+    print(mass)
+
+
+
+    

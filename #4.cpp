918d9b923c03c37707ff9edb7a44b736e266306e
Myroslav Trigbets <hello@myroslavtrigbets.com>
2018-01-14 19:52:33 +0200

    Update

--- #4.cpp
+++ #4.cpp
@@ -1,15 +1,15 @@
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
- 
+
 using namespace std;
- 
+
 int main()
 {
     double x1, y1, r1;
     double x2, y2, r2;
     cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
- 
+
 	if( x1 == x2 && y1 == y2 && r1 == r2 )
 	{
 		cout << -1 << endl;
@@ -26,7 +26,7 @@ int main()
 	{
 		cout << 0 << endl;
 	}
-	else 
+	else
 	{
 		cout << 2 << endl;
 	}

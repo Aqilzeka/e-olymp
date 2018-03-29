41d79f6646f631c3ab61d18a9c9467cb8934b725
Myroslav Trigubets <hello@myroslavtrigubets.com>
2018-03-29 21:55:58 +0300

    Update
    Signed-off-by: Bogdan Trigubets <bogdantrigubets@icloud.com>

--- 2000-2999/#2671.py
+++ 2000-2999/#2671.py
@@ -18,7 +18,7 @@ for i in range(n):
     for j in range(m):
         if mass[i][j] == -1:
 
-            file_w.write("* ")
+            file_w.write("*")
             #print("*", end=" ")
         else:
             if (i > 0 and  mass[i - 1][j] == -1):
@@ -37,13 +37,14 @@ for i in range(n):
                 mass[i][j] += 1
             if (i < n - 1 and j < m - 1 and mass[i + 1][j + 1] == -1):
                 mass[i][j] += 1
-            file_w.write(str(chr(mass[i][j] + 48)))
+            #file_w.write(str(chr(mass[i][j] + 48)))
+            file_w.write(str(mass[i][j]))
             #print(chr(mass[i][j] + 48), end="")
-            if j < m - 1:
-                file_w.write(" ")
+        if j < m - 1:
+            file_w.write(" ")
                 #print(" ", end="")
     if i != n -1:
         file_w.write("\n")
     #print()
 
-w.close()
+file_w.close()

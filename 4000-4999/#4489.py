40636919b6ced989958fdc4f8c4287847b6a6963
Myroslav Trigubets <hello@myroslavtrigubets.com>
2018-03-30 19:39:34 +0300

    Update
    Signed-off-by: Bogdan Trigubets <bogdantrigubets@icloud.com>

--- 4000-4999/#4489.py
+++ 4000-4999/#4489.py
@@ -1,3 +1,18 @@
+def function(mass):
+    count = 0
+    for i in range(len(mass)):
+        if i % 2 == 0:
+            count += mass[i]
+        else:
+            count -= mass[i]
+
+    if count % 11 == 0:
+        flag = True
+    else:
+        flag = False
+
+    return flag
+
 r_file = open("input.txt", "r").read().split("\n")
 w_file = open("output.txt", "w")
 n = list(r_file[0])
@@ -15,11 +30,11 @@ for i in range(m):
     if i != 0:
         w_file.write("\n")
     if mass[i][0] == 1:
-        temp = ""
+        temp = []
         for j in range(mass[i][1] - 1, mass[i][2]):
-            temp += n[j]
+            temp.append(int(n[j]))
         #print("---------", temp)
-        if int(temp) % 11 == 0:
+        if function(temp):
             w_file.write("YES")
         else:
             w_file.write("NO")

8f2f600fb45aa9546c9d33dbfbaa32ec3e429394
Myroslav Trigubets <hello@myroslavtrigubets.com>
2018-03-30 18:46:58 +0300

    Update
    Signed-off-by: Bogdan Trigubets <bogdantrigubets@icloud.com>

---
+++ 4000-4999/#4489.py
@@ -0,0 +1,29 @@
+r_file = open("input.txt", "r").read().split("\n")
+n = list(r_file[0])
+m = int(r_file[1])
+mass = []
+for i in range(2, m + 2):
+    mass.append([])
+    temp = r_file[i].split()
+    mass[i - 2].append(int(temp[0]))
+    mass[i - 2].append(int(temp[1]))
+    mass[i - 2].append(int(temp[2]))
+print(n,mass)
+for i in range(m):
+    print("i = ",i, "type = ",mass[i][0])
+
+    if mass[i][0] == 1:
+        temp = ""
+        for j in range(mass[i][1] - 1, mass[i][2]):
+            temp += n[j]
+        print("---------", temp)
+        if int(temp) % 11 == 0:
+            print("YES")
+        else:
+            print("NO")
+    elif mass[i][0] == 2:
+        if n[mass[i][1] - 1] == str(mass[i][2]):
+            print("YES", end="")
+        else:
+            print("NO", end="")
+    print()

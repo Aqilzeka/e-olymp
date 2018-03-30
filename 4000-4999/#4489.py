92fccf1d6a1481cf64276741f3ad7a3e67f5832e
Myroslav Trigubets <hello@myroslavtrigubets.com>
2018-03-30 19:26:40 +0300

    Update
    Signed-off-by: Bogdan Trigubets <bogdantrigubets@icloud.com>

--- 4000-4999/#4489.py
+++ 4000-4999/#4489.py
@@ -1,4 +1,5 @@
 r_file = open("input.txt", "r").read().split("\n")
+w_file = open("output.txt", "w")
 n = list(r_file[0])
 m = int(r_file[1])
 mass = []
@@ -8,22 +9,25 @@ for i in range(2, m + 2):
     mass[i - 2].append(int(temp[0]))
     mass[i - 2].append(int(temp[1]))
     mass[i - 2].append(int(temp[2]))
-print(n,mass)
+#print(n,mass)
 for i in range(m):
-    print("i = ",i, "type = ",mass[i][0])
-
+    #print("i = ",i, "type = ",mass[i][0])
+    if i != 0:
+        w_file.write("\n")
     if mass[i][0] == 1:
         temp = ""
         for j in range(mass[i][1] - 1, mass[i][2]):
             temp += n[j]
-        print("---------", temp)
+        #print("---------", temp)
         if int(temp) % 11 == 0:
-            print("YES")
+            w_file.write("YES")
         else:
-            print("NO")
+            w_file.write("NO")
     elif mass[i][0] == 2:
         if n[mass[i][1] - 1] == str(mass[i][2]):
-            print("YES", end="")
+            w_file.write("YES")
         else:
-            print("NO", end="")
-    print()
+            w_file.write("NO")
+    #print()
+
+w_file.close()
